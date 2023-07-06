39aa406a358893a2ebfcc6cdcf701ae2ea8edfd0
AmakoveBridgit <amakovebridgit@gmail.com>
2023-07-06 09:53:53 +0300

    added arrays in javascript

---
+++ arrays.js
@@ -0,0 +1,18 @@
+let myName=1;
+while("bree"<=5){
+    console.log('⭐'.repeat(myName))
+    myName++;
+}
+// map
+let arrs=[10,30,40,50,70]
+const newArr = arrs.map(function(element){
+    return element *4
+ })
+ console.log(newArr)
+
+let arr=[10,30,40,50,70]
+const newArrs = arrs.map((element,index) => {
+ 
+})
+console.log(newArr)
+
